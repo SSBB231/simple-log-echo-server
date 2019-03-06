e03f407f4e859ceacae0d26f73b8ea71b8d35bb4
Jimmy Pineda <jimmy.pineda@sap.com>
2019-03-06 11:54:09 -0600

    Added runnable and executor thread pool

--- src/main/java/com/mycompany/app/SimpleLogEchoServer.java
+++ src/main/java/com/mycompany/app/SimpleLogEchoServer.java
@@ -10,6 +10,8 @@ import java.sql.Timestamp;
 import java.util.Date;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.sound.sampled.SourceDataLine;
 
@@ -18,9 +20,11 @@ public class SimpleLogEchoServer implements LogEchoServer{
     ServerSocket serverSocket;
     int contentLength = 0;
     int connectionCount;
+    ExecutorService executor;
 
     public SimpleLogEchoServer(){
         connectionCount = 0;
+        executor = Executors.newCachedThreadPool();
     }
 
     private void resetConnectionCount(){
@@ -58,121 +62,7 @@ public class SimpleLogEchoServer implements LogEchoServer{
     }
 
     public void serviceConnection(Socket clientSocket) throws IOException{
-
-        if(clientSocket == null){
-            return;
-        }
-
-        Date now = new Date();
-
-        // Create read and write streams
-        InputStreamReader clientInput = new InputStreamReader(clientSocket.getInputStream());
-        BufferedReader clientInputReader = new BufferedReader(clientInput);
-        PrintWriter clientWriter = new PrintWriter(clientSocket.getOutputStream());
-
-        // initialize
-        contentLength = 0;
-
-        String headers = readHeaders(clientInputReader);
-        String body = readBody(clientInputReader, contentLength);
-
-        System.out.println(String.format("\nConnection on port %d opened", clientSocket.getPort()));
-        System.out.println(String.format("---------------This is the Request --------------------------\n%s", now.toString()));
-        System.out.println(String.format("This string came from %s", clientSocket.getInetAddress().getHostName()));
-        System.out.println(String.format("IP address of remote %s", clientSocket.getInetAddress().getHostAddress()));
-        System.out.println(String.format("Request number: %d", connectionCount));
-        System.out.println(String.format("Headers size: %d", headers.length()));
-        System.out.println(String.format("Body size: %d", body.length()));
-        System.out.println(String.format("Port: %d\n", clientSocket.getPort()));
-        System.out.print(headers);
-        System.out.println("\r");
-        System.out.println(body);
-        System.out.println(String.format("----------------------------------------------------------------"));
-
-        // Write response
-        StringBuilder response = new StringBuilder();
-        response.append("HTTP/1.1 200 OK\r\n");
-        response.append("Access-Control-Allow-Origin: *\n");
-        response.append("Access-Control-Allow-Headers: Content-Type\n");
-        response.append("Access-Control-Allow-Methods: POST, GET, OPTIONS\n");
-        response.append("Connection: close\r\n");
-        response.append(String.format("Content-Length: %d\r\n", headers.length()+body.length()+2));
-        response.append(String.format("Content-Type: %s\r\n", "text/plain"));
-        response.append(String.format("\r\n%s\r\n%s", headers, body));
-
-        printOutResponse(response);
-
-        try {
-            Thread.sleep(3000);
-        } catch (Exception e){
-            System.out.println("Some concurrent error happened:\n" + e.getMessage());
-        }
-
-        clientWriter.print(response.toString());
-        clientWriter.flush();
-
-        clientInputReader.close();
-        clientWriter.close();
-
-        System.out.println(String.format("Connection on port %d closed", clientSocket.getPort()));
-    }
-
-    private void printOutResponse(StringBuilder response){
-        System.out.println("\nResponse to Send");
-        System.out.println("----------------------------------------------------------------");
-        System.out.println(response.toString()+"\n");
-        System.out.println("----------------------------------------------------------------");
-    }
-
-    private int extractContentLength(String line){
-        Scanner scanner = new Scanner(line);
-        scanner.useDelimiter(" ");
-        try {
-            scanner.next();
-            return scanner.nextInt();
-        } catch (InputMismatchException e){
-            return 0;
-        }
-    }
-
-    private String readHeaders(BufferedReader reader) throws IOException{
-
-        String line = reader.readLine();
-        StringBuilder retval = new StringBuilder();
-
-        // Read headers
-        while(line != null && !line.isEmpty()){
-
-            if(line.toLowerCase().startsWith("content-length: ")){
-                contentLength = extractContentLength(line);
-            }
-
-            retval.append(String.format("%s\r\n", line));
-            System.out.println(line);
-            line = reader.readLine();
-        }
-
-        return retval.toString();
-    }
-
-    private String readBody(BufferedReader clientInputReader, int length) throws IOException{
-
-
-        if(length <= 0){
-            return "";
-        }
-
-        String line;
-
-        char[] bodyBytes = new char[length];
-        int bytesRead = clientInputReader.read(bodyBytes);
-
-        if(bytesRead <= 0){
-            return "";
-        }
-
-        line = new String(bodyBytes);
-
-        return line;
+        Runnable requestServicer = new RequestServicer(clientSocket, connectionCount);
+        executor.execute(requestServicer);
     }
 }
